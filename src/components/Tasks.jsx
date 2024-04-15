03ffe6c54ec09bf7b85763b1dd84ff5e6bc15b35
Andre Santos <andressouza25@gmail.com>
2024-04-15 16:07:03 -0300

    style: add sass and change font family

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,11 @@
+const Tasks = () => {
+    return (
+        <div className="tasks-container">
+            <h3>Últimas tarefas</h3>
+
+            <div className="last-tasks"></div>
+        </div>
+    );
+};
+
+export default Tasks;
