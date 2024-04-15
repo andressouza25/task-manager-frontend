1094e372c9b265a253f60568b7fb4123a366baf0
Andre Santos <andressouza25@gmail.com>
2024-04-15 17:56:14 -0300

    feat: add task item component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./Tasks.scss";
+
 const Tasks = () => {
+    const [tasks, setTasks] = useState([]);
+
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get(
+                "https://santos-task-manager.up.railway.app/tasks"
+            );
+            setTasks(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
     return (
         <div className="tasks-container">
-            <h3>Últimas tarefas</h3>
+            <h2>Minhas Tarefas</h2>
 
-            <div className="last-tasks"></div>
+            <div className="last-tasks">
+                <h3>Últimas Tarefas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => task.isCompleted === false)
+                        .map((lastTask) => (
+                            <p>{lastTask.description}</p>
+                        ))}
+                </div>
+            </div>
+
+            <div className="completed-tasks">
+                <h3>Tarefas Concluídas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => task.isCompleted)
+                        .map((completedTask) => (
+                            <p>{completedTask.description}</p>
+                        ))}
+                </div>
+            </div>
         </div>
     );
 };
