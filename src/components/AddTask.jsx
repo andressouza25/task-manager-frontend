304e7547e3baa989b35fa03754ac78ce2599a488
Andre Santos <andressouza25@gmail.com>
2024-04-16 16:22:38 -0300

    feat: fetch tasks after addition

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -8,7 +8,7 @@ import CustomButton from "./CustomButton";
 
 import "./AddTask.scss";
 
-const AddTask = () => {
+const AddTask = ({ fetchTasks }) => {
     const [task, setTask] = useState("");
 
     const alert = useAlert();
@@ -32,7 +32,13 @@ const AddTask = () => {
                     isCompleted: false,
                 }
             );
-        } catch (error) {}
+
+            await fetchTasks();
+
+            setTask("");
+        } catch (error) {
+            alert.error("Algo deu errado.");
+        }
     };
 
     return (
