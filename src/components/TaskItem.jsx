4bb43ca62c5b5ed797c7bc9f6925357e329e8c87
Andre Santos <andressouza25@gmail.com>
2024-04-17 16:02:09 -0300

    feat: add function to delete a task

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,8 +1,20 @@
+import { AiFillDelete } from "react-icons/ai";
+import axios from "axios";
+import { useAlert } from "react-alert";
+
 import "./TaskItem.scss";
 
-import { AiFillDelete } from "react-icons/ai";
-
 const TaskItem = ({ task }) => {
+    const handleTaskDeletion = async () => {
+        try {
+            await axios.delete(
+                `https://santos-task-manager.up.railway.app/tasks/${task._id}`
+            );
+        } catch (error) {
+            alert.error("Algo deu errado");
+        }
+    };
+
     return (
         <div className="task-item-container">
             <div className="task-description">
@@ -28,7 +40,11 @@ const TaskItem = ({ task }) => {
                 </label>
             </div>
             <div className="delete">
-                <AiFillDelete size={18} color="#F97474" />
+                <AiFillDelete
+                    size={18}
+                    color="#F97474"
+                    onClick={handleTaskDeletion}
+                />
             </div>
         </div>
     );
