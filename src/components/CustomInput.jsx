6094d9a295d2e1222b8381f3e0961614f00ad8af
Andre <andressouza25@gmail.com>
2024-04-25 14:46:20 -0300

    test

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -8,27 +8,23 @@ const CustomInput = ({ label, value, onChange, onEnterPress }) => {
   }
 
   return (
-        <div className="custom-input-container">
-            <input
-                type="text"
-                className="custom-input"
-                value={value}
-                onChange={(e) => onChange(e)}
-                onKeyDown={(e) => handleKeyDown(e)}
-            />
+    <div className='custom-input-container'>
+      <input
+        type='text'
+        className='custom-input'
+        value={value}
+        onChange={(e) => onChange(e)}
+        onKeyDown={(e) => handleKeyDown(e)}
+      />
 
-            {label
-              ? (
-                <label
-                    className={`${
-                        value.length > 0 ? 'shrink' : ''
-                    } custom-input-label`}
-                >
-                    {label}
-                </label>
-                )
-              : null}
-        </div>
+      {label ? (
+        <label
+          className={`${value.length > 0 ? 'shrink' : ''} custom-input-label`}
+        >
+          {label}
+        </label>
+      ) : null}
+    </div>
   )
 }
 
